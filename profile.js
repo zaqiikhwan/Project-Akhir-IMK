4e869b5e26caca515fe1d710e10da4c7049f0443
Achmad Galih <achmadgalihwp9503@gmail.com>
2022-12-16 14:58:56 +0700

    refactor code

---
+++ profile.js
@@ -0,0 +1,17 @@
+        let subMenu = document.getElementById("subMenu");
+
+        toggleMenu = () => {
+            subMenu.classList.toggle("open-menu");
+        };
+
+        const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#password");
+        
+        togglePassword.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
