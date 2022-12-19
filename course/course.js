5c47fbfca12f3f16bc917b75d64cb659e0a0bf12
Muh Zaqi <zaqiikhwan22@gmail.com>
2022-12-19 12:56:10 +0700

    refactor all of things

--- course/course.js
+++ course/course.js
@@ -39,7 +39,7 @@ let data = [
 data.forEach((content, index) => {
     let courseElement = `
         <div class="video" data-id="${content.id}">
-            <img src="assets-branch-course/image/play-icon-white.svg" alt="">
+            <img src="../assets-branch-course/image/play-icon-white.svg" alt="">
             <p>0${index + 1}</p>
             <p class="title">${content.title}</p>
         </div>
@@ -49,22 +49,22 @@ data.forEach((content, index) => {
 
 let contents = document.querySelectorAll('.video');
 contents[0].classList.add('active');
-contents[0].querySelector('img').src = 'assets-branch-course/image/stop-icon-white.svg';
+contents[0].querySelector('img').src = '../assets-branch-course/image/stop-icon-white.svg';
 
 contents.forEach(selected_content => {
     selected_content.onclick = () => {
 
         for (all_contents of contents) {
             all_contents.classList.remove('active');
-            all_contents.querySelector('img').src = 'assets-branch-course/image/play-icon-blue.svg';
+            all_contents.querySelector('img').src = '../assets-branch-course/image/play-icon-blue.svg';
 
         }
 
         selected_content.classList.add('active');
-        selected_content.querySelector('img').src = 'assets-branch-course/image/stop-icon-white.svg';
+        selected_content.querySelector('img').src = '../assets-branch-course/image/stop-icon-white.svg';
 
         let match_content = data.find(content => content.id == selected_content.dataset.id);
-        content.src = 'assets-branch-course/video/' + match_content.name;
+        content.src = '../assets-branch-course/video/' + match_content.name;
         contentTitle.innerHTML = match_content.title;
     }
 });
